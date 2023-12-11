b810ee935f248a643128810a4bb8914b601ee73a
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-11 13:02:43 +0300

    some reorganizing of the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_bugkill/home_page.dart';
+import 'package:project_bugkill/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
