98dfb869ad5f380c00c33287d1120291d24159ec
zheer barzan <zheerbarzan85@outlook.com>
2024-07-30 19:57:34 +0300

    adding the structure of the application

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:project_whisper/services/auth/auth_service.dart';
+import 'package:project_whisper/pages/settings_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+  void logout() {
+    final authService = AuthService();
+    authService.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              //logo
+              DrawerHeader(
+                child: Icon(Icons.message,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    size: 50),
+              ),
+              //home list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  title: const Text("H O M E"),
+                  leading: const Icon(Icons.home),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+              //settings list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  title: const Text("S E T T I N G S"),
+                  leading: const Icon(Icons.settings),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SettingsPage()));
+                  },
+                ),
+              ),
+            ],
+          ),
+          //logout
+          Padding(
+            padding: const EdgeInsets.only(left: 25, bottom: 25),
+            child: ListTile(
+              title: const Text("L O G O U T"),
+              leading: const Icon(Icons.logout),
+              onTap: logout,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
