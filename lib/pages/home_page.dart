dcf53b4179079d281a7f1994cb89131fc5bde80b
zheer barzan <zheerbarzan85@outlook.com>
2024-08-25 19:36:57 +0300

    adding the user instance

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:project_bugkill/components/my_drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // make an instance of the user
+  final currentUser = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("H O M E"),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        elevation: 10,
       ),
-      drawer: MyDrawer(),
-      body: Container(
-        color: Theme.of(context).colorScheme.primary,
+      drawer: const MyDrawer(),
+      body: Center(
+        child: Column(
+          children: [
+            //the bug
+
+            // post message
+
+            // login as
+            Text("Logged in as ${currentUser.email!}"),
+          ],
+        ),
       ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
     );
   }
 }
