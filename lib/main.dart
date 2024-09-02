5defbe4e3589741ef1aae136e8e854e1067920b2
Zheer Barzan <zheerbarzan85@outlook.com>
2024-09-02 20:35:04 +0300

    adding the splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:project_bugkill/services/auth_gate.dart';
 import 'package:project_bugkill/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -7,12 +8,32 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(ChangeNotifierProvider(
       create: (context) => ThemeProvider(), child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    intializetion();
+  }
+
+  void intializetion() async {
+    print("pausing...");
+    await Future.delayed(const Duration(seconds: 3));
+    print("pausing done...");
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
