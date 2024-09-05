33d24f00229f41d8284b2a3091b5064e1b272cfe
zheer barzan <zheerbarzan85@outlook.com>
2024-09-05 18:40:35 +0300

    adding the comment button and redisinging the UI

--- lib/components/posts.dart
+++ lib/components/posts.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project_bugkill/components/comment_button.dart';
 import 'package:project_bugkill/components/like_button.dart';
 
 class BugPost extends StatefulWidget {
@@ -95,58 +96,62 @@ class _BugPostState extends State<BugPost> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
-      padding: const EdgeInsets.all(25),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        children: [
-          //profile picture
-          Container(
-            decoration: BoxDecoration(
-                shape: BoxShape.circle, color: Colors.grey.shade400),
-            padding: const EdgeInsets.all(10),
-            child: Icon(
-              Icons.person,
-              color: Theme.of(context).colorScheme.inversePrimary,
+        margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
+        padding: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.grey.shade400),
+                  padding: const EdgeInsets.all(10),
+                  child: Icon(
+                    Icons.person,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  widget.username,
+                  style: TextStyle(color: Colors.grey.shade600),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.username,
-                style: TextStyle(color: Colors.grey.shade600),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(widget.message),
-            ],
-          ),
-
-          const Spacer(),
-          Column(
-            children: [
-              // like button
-              LikeButton(isLiked: isLiked, onTap: toggleLike),
-              const SizedBox(
-                height: 10,
-              ),
-              // like count
-              Text(
-                widget.likes.length.toString(),
-                style: TextStyle(color: Colors.grey.shade600),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+            const SizedBox(
+              height: 20,
+            ),
+            Text(widget.message),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    LikeButton(isLiked: isLiked, onTap: toggleLike),
+                    Text(widget.likes.length.toString()),
+                  ],
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  children: [
+                    CommentButton(onTap: showCommentDialog),
+                    const Text("0"),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
